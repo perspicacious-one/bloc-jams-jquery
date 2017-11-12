c0c6263beba95c20089bfc5e386743371d49a8f6
perspicacious-one <nelsondcraig@gmail.com>
2017-11-11 22:02:14 -0800

    extra credit (pretty time)

--- scripts/helper.js
+++ scripts/helper.js
@@ -3,7 +3,7 @@ class Helper {
   playPauseAndUpdate(song) {
     player.playPause(song);
     if (player.playState === 'stopped') { return; }
-    const totalTime = player.currentlyPlaying.duration;
+    const totalTime = player.prettyTime(player.currentlyPlaying.duration);
     $('#time-control .total-time').text(totalTime);
   }
 }
